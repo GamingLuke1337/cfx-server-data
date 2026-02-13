fe1787d1a2ec1515075278019edf2ef75ecbd936
Lukas Döring <64887613+GamingLuke1337@users.noreply.github.com>
2026-02-13 14:26:24 +0100

    cleanup

--- [gamemodes]/basic-gamemode/fxmanifest.lua
+++ [gamemodes]/basic-gamemode/fxmanifest.lua
@@ -1,14 +1,16 @@
 -- This resource is part of the default Cfx.re asset pack (cfx-server-data)
 -- Altering or recreating for local use only is strongly discouraged.
 
-version("2.0.1")
-author("Cfx.re <root@cfx.re>")
-description("A basic freeroam gametype that uses the default spawn logic from spawnmanager.")
-repository("https://github.com/bitpredator/cfx-server-data")
+fx_version 'cerulean'
+game 'common'
 
-resource_type("gametype")({ name = "Freeroam" })
+version '2.0.1'
+author 'Cfx.re <root@cfx.re>'
+description 'A basic freeroam gametype that uses the default spawn logic from spawnmanager.'
+repository 'https://github.com/citizenfx/cfx-server-data'
 
-client_script("basic_client.lua")
+resource_type 'gametype' { name = 'Freeroam' }
 
-game("common")
-fx_version("cerulean")
+client_script 'basic_client.lua'
+
+dependency 'spawnmanager'
